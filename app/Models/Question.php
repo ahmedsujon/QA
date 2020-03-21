cf5880d3e70c5e94ff57005302a0ffe82f9815c9
ahmedsujon <ahmedsujon050@gmail.com>
2020-03-21 08:19:04 +0600

    Questions Relationship

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable = [
-        'title', 'slug', 'body', 'view', 'answer', 'votes', 'best_answer_id', 'user_id',
+        'title', 'body',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
